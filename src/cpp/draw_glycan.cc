3d85c667106f6ab0a99d518b32ea47e6604a8d0c
Robert Davis <rtdavis22@gmail.com>
2012-01-18 18:09:07 -0500

    Added more options for creating images of glycans.

--- src/cpp/draw_glycan.cc
+++ src/cpp/draw_glycan.cc
@@ -1,3 +1,8 @@
+// Author: Robert Davis
+//
+// This program outputs a graphviz file representing the glycan specified in
+// condensed glycam notation on the command line.
+
 #include "gmml/gmml.h"
 
 #include <iostream>
@@ -11,15 +16,20 @@ int main(int argc, char **argv) {
     GlycanDrawer drawer;
 
     for (int i = 1; i < argc - 1; i++) {
-        if (string(argv[i]) == "-edgelabels") {
+        string arg(argv[i]);
+        if (arg == "-edgelabels") {
             drawer.show_edge_labels();
-        } else if (string(argv[i]) == "-dpi") {
+        } else if (arg == "-dpi") {
             try {
                 drawer.set_dpi(convert_string<int>(string(argv[i + 1])));
             } catch(...) {
                 // Don't complain.
             }
             i++;
+        } else if (arg == "-hide_config_labels") {
+            drawer.hide_config_labels();
+        } else if (arg == "-hide_position_labels") {
+            drawer.hide_position_labels();
         }
     }
 

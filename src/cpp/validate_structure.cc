85749f1efd2094ca21d9854db8095bce6b996d02
Robert Davis <rtdavis22@gmail.com>
2012-01-18 19:14:33 -0500

    Added documentation to the structure validation program.

--- src/cpp/validate_structure.cc
+++ src/cpp/validate_structure.cc
@@ -1,24 +1,38 @@
+// Author: Robert Davis
+//
+// This program checks to make sure a particular sequence in GLYCAM condensed nomenclature has the
+// correct sequence and can be built. The sequence is taken as a command-line argument. If there is
+// an error, the program writes an error message to standard output and returns with a non-zero exit
+// status.
+
 #include "config.h"
 
-#include <fstream>
+#include <exception>
 #include <iostream>
-#include <sstream>
+#include <string>
 
 #include "gmml/gmml.h"
 
-using namespace std;
-using namespace gmml;
-
 int main(int argc, char *argv[]) {
-    add_path(string(PROJECT_ROOT) + "dat/prep_files/");
+    gmml::add_path(std::string(PROJECT_ROOT) + "dat/prep_files/");
     
-    load_prep_file("Glycam_06.prep");
-    load_prep_file("Neu5Gc_a_06.prep");
-    load_prep_file("sulfate.prep");
-    load_prep_file("ACE.prep");
-    load_prep_file("MEX.prep");
+    gmml::load_prep_file("Glycam_06.prep");
+    gmml::load_prep_file("Neu5Gc_a_06.prep");
+    gmml::load_prep_file("sulfate.prep");
+    gmml::load_prep_file("ACE.prep");
+    gmml::load_prep_file("MEX.prep");
     
-    string input(argv[1]);
-  
-    Structure *structure = glycam_build(input);
+    if (argc < 2) {
+        return -1;
+    }
+    std::string input(argv[1]);
+
+    try {
+        gmml::Structure *structure = gmml::glycam_build(input);
+    } catch(const std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+
+    return 0;
 }

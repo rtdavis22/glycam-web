2030e7ad99ab6371d58265bc48588d775936d2f1
Robert Davis <rtdavis22@gmail.com>
2012-01-18 20:15:08 -0500

    Added oligosaccharide library JavaBean.

---
+++ src/java/src/oligosaccharide_library/Item.java
@@ -0,0 +1,36 @@
+// Author: Robert Davis
+
+package oligosaccharide_library;
+
+// This class represents a structure in the oligosaccharide library. It is suitable for use as
+// a JavaBean.
+public class Item {
+    // The unique id assigned to this sequence.
+    private int id;
+
+    // The sequence in GLYCAM condensed nomenclature.
+    private String sequence;
+
+    // A name for the structure.
+    private String name;
+
+    // Additional information about the structure.
+    private String description;
+
+    public Item(int id, String sequence, String name, String description) {
+        this.id = id;
+        this.sequence = sequence;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() { return id; }
+    public String getSequence() { return sequence; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+
+    public void setId(int id) { this.id = id; }
+    public void setSequence(String sequence) { this.sequence = sequence; }
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+}

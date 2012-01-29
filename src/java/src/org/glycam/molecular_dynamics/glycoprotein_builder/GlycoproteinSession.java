3e7306f21f8bf07ad64359ab2269f08b9c82c73c
Robert Davis <rtdavis22@gmail.com>
2012-01-28 22:29:33 -0500

    Change protocol buffer member to a Builder.

--- src/java/src/org/glycam/molecular_dynamics/glycoprotein_builder/GlycoproteinSession.java
+++ src/java/src/org/glycam/molecular_dynamics/glycoprotein_builder/GlycoproteinSession.java
@@ -1,10 +1,10 @@
 package org.glycam.molecular_dynamics.glycoprotein_builder;
 
+import org.glycam.CPP;
+import org.glycam.Logging;
 import org.glycam.molecular_dynamics.glycoprotein_builder.PdbInfoPB.CYSPair;
 import org.glycam.molecular_dynamics.glycoprotein_builder.PdbInfoPB.PdbInfo;
 import org.glycam.molecular_dynamics.glycoprotein_builder.PdbInfoPB.PdbResidueInfo;
-import org.glycam.CPP;
-import org.glycam.Logging;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,22 +14,25 @@ import java.util.List;
 public class GlycoproteinSession {
     private File pdbFile;
 
-    private PdbInfo pdbInfo;
+    private PdbInfo.Builder pdbInfoBuilder;
 
     public GlycoproteinSession(File pdbFile) {
         this.pdbFile = pdbFile;
         initializePdbInfo();
     }
 
-    public List<PdbResidueInfo> getHisResidues() { return pdbInfo.getHisResidueList(); }
-    public List<CYSPair> getCloseCYSPairs() { return pdbInfo.getCloseCysPairList(); }
-    public List<PdbResidueInfo> getAsnResidues() { return pdbInfo.getAsnResidueList(); }
+    public PdbInfo.Builder getPdbInfoBuilder() { return pdbInfoBuilder; }
+
+    public List<PdbResidueInfo> getHisResidues() { return pdbInfoBuilder.getHisResidueList(); }
+    public List<CYSPair> getCloseCYSPairs() { return pdbInfoBuilder.getCloseCysPairList(); }
+    public List<PdbResidueInfo> getAsnResidues() { return pdbInfoBuilder.getAsnResidueList(); }
 
     private void initializePdbInfo() {
         String command = "get_pdb_info " + pdbFile.getPath();
         try {
             Process process = CPP.exec(command);
-            pdbInfo = PdbInfo.parseFrom(process.getInputStream());
+            PdbInfo pdbInfo = PdbInfo.parseFrom(process.getInputStream());
+            this.pdbInfoBuilder = PdbInfo.newBuilder(pdbInfo);
             Logging.logger.info("Process " + command + " exited with value " + process.waitFor());
         } catch (IOException e) {
             Logging.logger.severe(e.getMessage());
